923d3ef6e5dda2c0b994483fea6f7593a2cfa0ae
songli zhao <songli.zhao@qq.com>
2023-04-15 14:18:22 +0800

    fix: update ip

--- chatgpt.py
+++ chatgpt.py
@@ -7,7 +7,7 @@ import json
 args = sys.argv
 prompt = args[1]
 
-url = 'http://100.26.242.75:8000/v1/chat/completions'
+url = 'http://54.174.76.97:8000/v1/chat/completions'
 
 data = {
     "model": "gpt-3.5-turbo",
